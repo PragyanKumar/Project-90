59a369fd62694ca6ec71cfcc2255608738ae283f
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2021-08-15 14:26:36 +0530

    Add files via upload

--- Play.js
+++ Play.js
@@ -13,17 +13,18 @@ document.getElementById("player_answer").innerHTML="answer turn- "+ player2_name
 // Send function//
 
 function send(){
-number1=document.getElementById("number1").value
-number2=document.getElementById("number2").value
+number1=document.getElementById("Number_input").value
+number2=document.getElementById("Number_input2").value
 actual_answer=parseInt(number1) * parseInt(number2)
-}
+
 question_number="<h4>" + number1 + "X" + number2 + "</h4>"
 input_box="<br>Answer : <input type='text' id='input_check_box'> "
 check_button="<br><br><button class='btn btn-info' onclick='check()'> Check </button>"
 row=question_number + input_box + check_button
 document.getElementById("output").innerHTML=row
-document.getElementById("number1").value=""
-document.getElementById("number2").value=""
+document.getElementById("Number_input").value=""
+document.getElementById("Number_input2").value=""
+}
 // Check function //
 
 question_turn="player1"
@@ -31,16 +32,15 @@ answer_turn="player2"
 
 function check(){
 get_answer=document.getElementById("input_check_box").value
-answer=get_answer.toLowerCase()
 console.log(answer)
 
 if(answer==word){
     if(answer_turn=="player1"){
-        player1_score=player1_score+1
+        update_player1_score=player1_score+1
         document.getElementById("player1_score").innerHTML=player1_score
     }
     else{
-        player2_score=player2_score+1
+        update_player2_score=player2_score+1
         document.getElementById("player2_score").innerHTML=player2_score
     }
 }
